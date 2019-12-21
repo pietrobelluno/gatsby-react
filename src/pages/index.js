10ee7ff7629a14ef66e96673d9dbc38a32081909
Pietro Belluno Pilau <pietrop@outlook.com>
2019-12-21 00:13:32 -0300

    gatsby installed and some components

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { Link } from "gatsby"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Home</h1>
+    <a href="about">About</a>
+    <Link to="about">About</Link>
+  </Layout>
+)
+
+export default IndexPage
