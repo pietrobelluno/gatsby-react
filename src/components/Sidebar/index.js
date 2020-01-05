330adf216899c67febd70a2daaca813464818311
Pietro Belluno Pilau <pietrop@outlook.com>
2020-01-05 01:09:58 -0300

    SocialLinks component with icons from boxicons

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Profile from "../Profile";
 import * as S from "./styled";
+import SocialLinks from "../SocialLinks";
 
 const Sidebar = () => {
   return (
     <S.SidebarWrapper>
       <Profile></Profile>
+      <SocialLinks></SocialLinks>
     </S.SidebarWrapper>
   );
 };
---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,18 @@
+const handle = "pietrobelluno";
+
+const links = [
+  {
+    label: "Github",
+    url: `https://github.com/${handle}`
+  },
+  {
+    label: "Linkedin",
+    url: `https://linkedin.com/${handle}`
+  },
+  {
+    label: "Instagram",
+    url: `https://www.instagram.com/${handle}`
+  }
+];
+
+export default links;
