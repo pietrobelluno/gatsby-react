358640ca1ee38a740c4e9c0e023f90e82eb1b10f
Pietro Belluno Pilau <pietrop@outlook.com>
2020-01-05 01:30:52 -0300

    MenuLinks component finished

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import Profile from "../Profile";
 import * as S from "./styled";
 import SocialLinks from "../SocialLinks";
+import MenuLinks from "../MenuLinks";
 
 const Sidebar = () => {
   return (
     <S.SidebarWrapper>
       <Profile></Profile>
       <SocialLinks></SocialLinks>
+      <MenuLinks></MenuLinks>
     </S.SidebarWrapper>
   );
 };
